aa634e3d3560f605014c757c055f5ed5de878d72
Anibal-Alpizar <anibal.alpizar14@gmail.com>
2023-05-06 21:41:38 -0600

    user details

--- app/users/[id]/page.jsx
+++ app/users/[id]/page.jsx
@@ -1,9 +1,23 @@
-import React from "react";
+async function getUser(id) {
+  console.log(id);
+  const res = await fetch(`https://reqres.in/api/users/${id}`);
+  const data = await res.json();
+  return data.data;
+}
 
-function UsersPage() {
+async function UsersPage({ params }) {
+  const user = await getUser(params.id);
+
   return (
     <div>
       <h1>User details</h1>
+      <div>
+        <img src={user.avatar} alt={user.first_name} />
+        <h3>
+          {user.id} {user.first_name} {user.last_name}
+          <p>{user.email}</p>
+        </h3>
+      </div>
     </div>
   );
 }
