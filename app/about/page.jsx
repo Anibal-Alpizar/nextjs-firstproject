84ceb82e17b7d7578087a70e33963150f911e207
Anibal-Alpizar <anibal.alpizar14@gmail.com>
2023-05-06 19:58:42 -0600

    pages

---
+++ app/about/page.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+function AboutPage() {
+  return (
+    <div>
+      <h1>About Page</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Et voluptatum
+        officia cumque reprehenderit amet quibusdam assumenda. Repellendus,
+        culpa quasi? Amet cum sequi a quos sunt maxime asperiores qui, rerum
+        repellendus cumque eum accusantium unde velit rem nobis magnam
+        architecto aliquid earum. Quisquam asperiores fuga quis hic esse modi
+        placeat facilis amet commodi temporibus tempora dolores eos rerum magnam
+        expedita atque magni consequuntur accusamus, recusandae quia! Provident
+        porro, numquam minus distinctio expedita cum commodi nam excepturi esse
+        repudiandae, nobis laborum. Aliquid eos consequatur quis laudantium,
+        officiis excepturi obcaecati nesciunt maxime, similique expedita veniam
+        laboriosam. Fugit aliquid delectus, ex quas soluta provident!
+      </p>
+    </div>
+  );
+}
+
+export default AboutPage;
---
+++ app/services/page.jsx
@@ -0,0 +1,17 @@
+function ServicesPage() {
+  return (
+    <div>
+      <h1>Services Page</h1>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Doloremque
+        unde facere nesciunt placeat doloribus perferendis sed corporis
+        voluptatem inventore deleniti, provident eaque fuga voluptatum sapiente
+        iusto aliquid nulla et asperiores, adipisci dolores quo sint. Voluptas
+        mollitia eius deleniti. Totam delectus aliquid, libero debitis a esse
+        aspernatur eum expedita molestiae ab.
+      </p>
+    </div>
+  );
+}
+
+export default ServicesPage;
